2ebff73b69b381c3e7d80781e31d7536f5169d85
Maksim Vasilets <maxvasilets@icloud.com>
2021-11-14 13:57:34 +0300

    add swagger

--- src/main/kotlin/me/example/demo/Application.kt
+++ src/main/kotlin/me/example/demo/Application.kt
@@ -2,10 +2,36 @@ package me.example.demo
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import springfox.documentation.builders.ApiInfoBuilder
+import springfox.documentation.builders.PathSelectors
 
+import springfox.documentation.builders.RequestHandlerSelectors
+import springfox.documentation.service.ApiInfo
+
+import springfox.documentation.spi.DocumentationType
+
+import springfox.documentation.spring.web.plugins.Docket
+import springfox.documentation.swagger2.annotations.EnableSwagger2
+
+
+@EnableSwagger2
 @SpringBootApplication
 class Application
 
 fun main(args: Array<String>) {
-	runApplication<Application>(*args)
+    runApplication<Application>(*args)
+}
+
+@Configuration
+class SpringFoxConfig {
+    @Bean
+    fun api(): Docket? {
+        return Docket(DocumentationType.SWAGGER_2)
+            .select()
+            .apis(RequestHandlerSelectors.basePackage("me.example.demo.controller"))
+            .paths(PathSelectors.any())
+            .build()
+    }
 }
